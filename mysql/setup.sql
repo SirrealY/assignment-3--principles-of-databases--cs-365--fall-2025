fd5e922665de2cbba56fc3934846150c8f955b38
SirrealY <143436688+SirrealY@users.noreply.github.com>
2025-11-14 18:08:49 -0500

    created user passwords_user
    gave permissions as well

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -12,7 +12,11 @@ USE student_passwords;
 
 -- Create database user and grant permissions
 
-DROP USER IF EXISTS
+DROP USER IF EXISTS 'passwords_user'@'localhost';
+CREATE USER 'passwords_user'@'localhost'
+GRANT ALL PRIVILEGES ON student_passwords.* TO 'passwords_user'@'localhost'
+FLUSH PRIVILEGES;
+
 -- Setup Password Encryption
 SET block_encryption_mode = 'aes-256-cbc';
 SET @key_str = UNHEX(SHA2('SEUZ', 512));
